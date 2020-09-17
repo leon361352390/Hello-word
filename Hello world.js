ce9ddc93ec94b412f012966b84bd98ac826f8321
leon361352390 <70986407+leon361352390@users.noreply.github.com>
2020-09-17 10:36:44 +0800

    Hello world.js

---
+++ Hello world.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello world</title>
+    <script type="text/javascript"></script>
+    </style>
+</head>
+<body>
+  <div>
+    Hello world
+  </div>
+</body>
+</html>
